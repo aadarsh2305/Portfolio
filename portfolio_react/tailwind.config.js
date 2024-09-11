e679659a840bfaa20042568ca2d78cc98ee2d20c
aadarsh2305 <aadarshlohari91111@gmail.com>
2024-09-11 19:25:20 +0530

    Navbar section added

---
+++ portfolio_react/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Scanning all JS/JSX/TS/TSX files for Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
